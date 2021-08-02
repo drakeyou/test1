4388064606f1e212182a5b1963e7f8ba69fd8289
yellowpack <moimailru2@mail.ru>
2021-08-02 13:42:13 +0300

    ello

---
+++ index.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>hereeeee</title>
+</head>
+<body>длолдо
+go to file bitch  я макаронынй монстр and i'm
+</body>
+</html>
+<?php
